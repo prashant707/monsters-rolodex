c2d56c64d6032af7464e33c1db24c360409ed1b3
prashant707 <prashantsj643@gmail.com>
2020-04-24 15:07:37 +0530

    Updates

---
+++ precache-manifest.69bb5a5674724e52da2a6d30e8b65ee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdc5056a60bb9a4b375937385e4dc8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d2470367f8015e502f2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c655a51673bcf606f747",
+    "url": "/static/js/2.e2ea4272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e2ea4272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2470367f8015e502f2",
+    "url": "/static/js/main.4e5354f0.chunk.js"
+  },
+  {
+    "revision": "ad591d0f4708f88e02d5",
+    "url": "/static/js/runtime-main.ca69820c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
